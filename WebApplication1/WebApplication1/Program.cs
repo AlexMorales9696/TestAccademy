3d3af9cf5709bcb05bccbce848da615169f863e1
student <student@ACADEMYNETPD09>
2022-05-27 12:32:05 +0200

    fatta classe db e conection

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,3 +1,5 @@
+using WebApplication1.SQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
- 
+builder.Services
+   .AddSingleton<IDataProvider, DataProvider>(_ =>
+       new DataProvider(builder.Configuration.GetConnectionString("Db")));
+
+var app = builder.Build();
 
 
 var app = builder.Build();
---
+++ WebApplication1/WebApplication1/SQL/DataProvider.cs
@@ -0,0 +1,45 @@
+﻿using WebApplication1.Model;
+using System.Data.SqlClient;
+namespace WebApplication1.SQL
+{
+    public class DataProvider : IDataProvider
+    {
+
+        private readonly string _connectionString;
+        public DataProvider(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public int Add(Spettatore spettatore)
+        {
+            var query = @"INSERT INTO Spectators
+                                   ([Nome]
+                                   ,[Cognome]
+                                   ,[Età]
+                                    ,[IdBiglietto])
+                             VALUES
+                                   (@Nome
+                                   ,@Cognome
+                                   ,@età
+                                   ,@IdBigliettoS";
+            
+            
+                using var connection = new SqlConnection(_connectionString);
+                connection.Open();
+                using var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("Name", spettatore.Nome);
+                command.Parameters.AddWithValue("Cognome", spettatore.Cognome);
+                command.Parameters.AddWithValue("Età", spettatore.Età);
+                command.Parameters.AddWithValue("IdBiglietto", spettatore.IdBigliettoS);
+
+            return Convert.ToInt32(command.ExecuteScalar());
+            }
+            
+
+        }
+
+
+
+    }
+
